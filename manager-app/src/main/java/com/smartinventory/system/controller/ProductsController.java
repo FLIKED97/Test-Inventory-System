eeb950780304ee9c0789af3591c0e4d6528c6fbe
FLIKED97 <91269654+FLIKED97@users.noreply.github.com>
2025-12-05 16:12:18 +0200

    ексцепшин + валідація + приклади з record. Добавив функції: створити, видалити, змінити товар.

--- manager-app/src/main/java/com/smartinventory/system/controller/ProductsController.java
+++ manager-app/src/main/java/com/smartinventory/system/controller/ProductsController.java
@@ -3,9 +3,12 @@ package com.smartinventory.system.controller;
 import com.smartinventory.system.controller.payload.NewProductPayload;
 import com.smartinventory.system.entity.Product;
 import com.smartinventory.system.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -25,13 +28,24 @@ public class ProductsController {
 
     @GetMapping("create")
     public String getNewProductPage(){
-
         return "catalogue/products/new_product";
     }
     @PostMapping("create")
-    public String createProduct(NewProductPayload payload){
-        Product product = this.productService.createProduct(payload.title(), payload.details());
-        return "redirect:/catalogue/products/%d".formatted(product.getId());
+    public String createProduct(@Valid NewProductPayload payload,
+                                BindingResult bindingResult,
+                                Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("payload", payload);
+            model.addAttribute("errors", bindingResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage)
+                    .toList());
+            return "catalogue/products/new_product";
+        } else {
+            Product product = this.productService.createProduct(payload.title(), payload.details());
+            return "redirect:/catalogue/products/%d".formatted(product.getId());
+
+        }
     }
 
 
