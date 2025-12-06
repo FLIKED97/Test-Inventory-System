edadd5759313b2b1a4215c317ad4ca3a8bc6b6e3
FLIKED97 <91269654+FLIKED97@users.noreply.github.com>
2025-12-06 22:35:07 +0200

    Налаштував початке REST api для подальшої розробки. Зміг нормально опрацювати помилки

--- catalogue-service/src/main/java/com/smartinventory/system/catalogue/controller/ProductsRestController.java
+++ catalogue-service/src/main/java/com/smartinventory/system/catalogue/controller/ProductsRestController.java
@@ -10,6 +10,7 @@ import org.springframework.context.MessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
@@ -26,8 +27,6 @@ public class ProductsRestController {
 
     private final ProductService productService;
 
-    private final MessageSource messageSource;
-
     @GetMapping()
     public List<Product> findProducts(){
         return this.productService.findAllProducts();
@@ -36,18 +35,15 @@ public class ProductsRestController {
     @PostMapping
     public ResponseEntity<?> createProduct(@Valid @RequestBody NewProductPayload payload,
                                                  BindingResult bindingResult,
-                                                 UriComponentsBuilder uriComponentsBuilder,
-                                                 Locale locale){
+                                                 UriComponentsBuilder uriComponentsBuilder)
+            throws BindException {
+
         if (bindingResult.hasErrors()){
-            ProblemDetail problemDetail = ProblemDetail
-                    .forStatusAndDetail(HttpStatus.BAD_REQUEST,
-                            this.messageSource.getMessage("errors.400.title", new Object[0]
-                                    ,"errors.400.title", locale));
-            problemDetail.setProperty("errors", bindingResult.getAllErrors().stream()
-                    .map(ObjectError::getDefaultMessage)
-                    .toList());
-            return ResponseEntity.badRequest()
-                    .body(problemDetail);
+            if(bindingResult instanceof BindException exception){
+                throw exception;
+            } else {
+                throw new BindException(bindingResult);
+            }
         } else {
            Product product = this.productService.createProduct(payload.title(), payload.details());
            return ResponseEntity
